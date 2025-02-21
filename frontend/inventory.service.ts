cd06cdaa70fa743ba8a26f8dc2dd5a4af939a69c
Sravani Seelam <123595425+Sravani0821@users.noreply.github.com>
2025-02-21 13:24:38 -0500

    Create inventory.service.ts

---
+++ frontend/inventory.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Item } from './item';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InventoryService {
+
+  private apiUrl = 'http://localhost:8080/api/inventory';
+
+  constructor(private http: HttpClient) { }
+
+  getItems(): Observable<Item[]> {
+    return this.http.get<Item[]>(this.apiUrl);
+  }
+
+  addItem(item: Item): Observable<Item> {
+    return this.http.post<Item>(this.apiUrl, item);
+  }
+
+  updateItem(id: number, item: Item): Observable<Item> {
+    return this.http.put<Item>(`${this.apiUrl}/${id}`, item);
+  }
+
+  deleteItem(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+}
